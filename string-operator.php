f5fa87099f8270eb8ba2204b09255e57a19c39a3
HirenGajjar <gajjarhiren111@gmail.com>
2023-09-13 13:59:12 -0700

    String Operator
    Applying this commit will add the sting operator file with new value assigned to prefix and name variable, in step 2 message is displayed without concatenation operator

---
+++ string-operator.php
@@ -0,0 +1,19 @@
+<?php 
+//Changing value to prefix and name
+$prefix  = 'Thank you';
+$name    = 'Hiren';
+// Displaying message without concatenation operator
+$message = "$prefix $name";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>String Operator</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2><?= $name ?>'s Order</h2>
+    <p><?= $message ?></p>
+  </body>
+</html>
